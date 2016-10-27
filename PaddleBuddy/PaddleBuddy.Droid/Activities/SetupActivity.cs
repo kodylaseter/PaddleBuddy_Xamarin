bb4912b8c06fe5d042e3714f516c58e56e681f0c
Kody Laseter <kodylaseter1@gmail.com>
2016-10-27 03:38:10 -0400

    initial commit

---
+++ PaddleBuddy/PaddleBuddy.Droid/Activities/SetupActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace PaddleBuddy.Droid.Activities
+{
+    [Activity(Label = "PaddleBuddy", MainLauncher = true, Icon = "@drawable/icon")]
+    public class SetupActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
+}
+
