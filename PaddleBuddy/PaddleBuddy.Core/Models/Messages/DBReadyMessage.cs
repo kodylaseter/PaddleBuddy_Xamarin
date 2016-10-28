1cfbef250fec5a7812bb6a985ab30aaa5c32a700
Kody Laseter <kodylaseter1@gmail.com>
2016-10-28 02:32:53 -0400

    attempting to add support library

--- PaddleBuddy/PaddleBuddy.Core/Models/Messages/DBReadyMessage.cs
+++ PaddleBuddy/PaddleBuddy.Core/Models/Messages/DBReadyMessage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PaddleBuddy.Core.Models.Messages
+﻿namespace PaddleBuddy.Core.Models.Messages
 {
     public class DbReadyMessage
     {
--- PaddleBuddy/PaddleBuddy.Droid/Activities/MainActivity.cs
+++ PaddleBuddy/PaddleBuddy.Droid/Activities/MainActivity.cs
@@ -1,10 +1,11 @@
 using Android.App;
 using Android.OS;
+using Android.Support.V7.App;
 
 namespace PaddleBuddy.Droid.Activities
 {
     [Activity(Label = "MainActivity")]
-    public class MainActivity : Activity
+    public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
