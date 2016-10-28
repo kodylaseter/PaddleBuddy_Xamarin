28d1c7e70afd7490f3f14439d6137e8bb4385489
Kody Laseter <kodylaseter1@gmail.com>
2016-10-28 18:43:09 -0400

    working nav drawer

--- PaddleBuddy/PaddleBuddy.Droid/Activities/MainActivity.cs
+++ PaddleBuddy/PaddleBuddy.Droid/Activities/MainActivity.cs
@@ -1,17 +1,80 @@
+using System;
 using Android.App;
 using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
 using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
 
 namespace PaddleBuddy.Droid.Activities
 {
-    [Activity(Label = "MainActivity")]
-    public class MainActivity : AppCompatActivity
+    [Activity(Label = "MainActivity", Theme = "@style/AppTheme.NoActionBar")]
+    public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
     {
+        private Toolbar _toolbar;
+        private DrawerLayout _drawer;
+        private NavigationView _navigationView;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
             SetContentView(Resource.Layout.activity_main);
+
+            _toolbar = (Toolbar) FindViewById(Resource.Id.toolbar);
+            SetSupportActionBar(_toolbar);
+
+            _drawer = (DrawerLayout) FindViewById(Resource.Id.drawer_layout);
+            var toggle = new ActionBarDrawerToggle(this, _drawer, _toolbar, Resource.String.navigation_drawer_open,
+                Resource.String.navigation_drawer_close);
+            _drawer.SetDrawerListener(toggle);
+            toggle.SyncState();
+
+            _navigationView = (NavigationView) FindViewById(Resource.Id.nav_view);
+            _navigationView.SetNavigationItemSelectedListener(this);
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem menuItem)
+        {
+            int id;
+            if (menuItem == null)
+            {
+                id = Resource.Id.nav_map;
+                _navigationView.Menu.GetItem(0).SetChecked(true);
+            }
+            else
+            {
+                id = menuItem.ItemId;
+            }
+            Type fragmentType = null;
+            switch (id)
+            {
+                case Resource.Id.nav_map:
+                    //set fragmentType
+                    break;
+                case Resource.Id.nav_plan:
+                    //set fragmentType
+                    break;
+                default:
+                    //set to default
+                    break;
+            }
+
+            _drawer.CloseDrawer(GravityCompat.Start);
+            return true;
+        }
+
+        public override void OnBackPressed()
+        {
+            if (_drawer.IsDrawerOpen(GravityCompat.Start))
+            {
+                _drawer.CloseDrawer(GravityCompat.Start);
+            }
+            else
+            {
+                base.OnBackPressed();
+            }
         }
     }
 }
