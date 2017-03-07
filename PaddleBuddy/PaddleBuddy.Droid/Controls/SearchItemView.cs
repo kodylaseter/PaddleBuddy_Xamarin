5192a800b08025bcc0115d13023f10f70fad95fc
Kody Laseter <kodylaseter1@gmail.com>
2017-03-07 15:44:52 +0100

    more work on mainsearch; need to add close icon

--- PaddleBuddy/PaddleBuddy.Droid/Controls/SearchItemView.cs
+++ PaddleBuddy/PaddleBuddy.Droid/Controls/SearchItemView.cs
@@ -15,29 +15,10 @@ namespace PaddleBuddy.Droid.Controls
 
         private void Initialize()
         {
-            LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent);
-            Orientation = Orientation.Horizontal;
-            var padding = (int) Resources.GetDimension(Resource.Dimension.search_inner_padding);
-            SetPadding(padding, padding, padding, padding);
-            IconImageView = new ImageView(Context)
-            {
-                LayoutParameters = new LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.MatchParent)
-            };
-            ItemTextView = new TextView(Context)
-            {
-                LayoutParameters =
-                    new LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent)
-            };
-            ItemTextView.SetTextAppearance(Resource.Style.textMediumGray);
-            ItemTextView.Gravity = GravityFlags.CenterVertical;
-            var dividerView = new View(Context)
-            {
-                LayoutParameters = new LayoutParams(1, 8)
-            };
-            dividerView.SetBackgroundColor(new Color(ContextCompat.GetColor(Context, Resource.Color.search_divider)));
-            AddView(IconImageView);
-            AddView(dividerView);
-            AddView(ItemTextView);
+            AddView(Inflate(Context, Resource.Layout.view_searchitem, null));
+            ItemTextView = FindViewById<TextView>(Resource.Id.searchitem_textview);
+            IconImageView = FindViewById<ImageView>(Resource.Id.searchitem_imageview);
+            IconImageView.SetColorFilter(new Color(ContextCompat.GetColor(Context, Resource.Color.gray)), PorterDuff.Mode.SrcAtop);
         }
 
         public void UpdateData(SearchItem searchItem)
@@ -45,7 +26,6 @@ namespace PaddleBuddy.Droid.Controls
             SearchItem = searchItem;
             ItemTextView.Text = searchItem.SearchString;
             IconImageView.SetImageResource(Resource.Drawable.ic_place_black_24dp);
-            IconImageView.SetColorFilter(new Color(ContextCompat.GetColor(Context, Resource.Color.gray)), PorterDuff.Mode.SrcAtop);
         }
 
         public SearchItemView(Context context) : base(context)
